068ed39ab7cf3e29fa987aeb3b2644fa58f943d8
Ryan Truong <ryan.truong2304@gmail.com>
2022-03-22 17:46:34 +1100

    Added Book object from previous example and edited styling and html

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ addBtn.addEventListener('click', () => {
         popUp.classList.add('display-form');
     }
 });
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
