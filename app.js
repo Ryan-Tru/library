3a9dacf424b78888daf0945b1c20ca7479d84cbc
Ryan Truong <ryan.truong2304@gmail.com>
2022-03-22 23:45:03 +1100

    Got it somewhat working

--- app.js
+++ app.js
@@ -1,8 +1,75 @@
 const addBtn = document.querySelector('.add-btn');
 const popUp = document.querySelector('.pop-up-form');
+const readBtnYes = document.querySelector('#book-status-form-yes');
+const readBtnNo = document.querySelector('#book-status-form-no');
+const submitForm = document.querySelector('#submit-form');
+const bookContainer = document.querySelector('.books-container');
 
+let readStatus;
 
+let myLibrary = [
+];
 
+//Book constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+//This function adds a book to the library.
+//Using the Book constructor as the protoype.
+function addBookToLibrary() {
+    if(readStatus === undefined) return; 
+    const book = Object.create(Book);
+    book.title = document.getElementById('book-title-form').value;
+    book.author = document.getElementById('book-author-form').value;
+    book.pages = document.getElementById('book-pages-form').value;
+    book.read = readStatus;
+
+    document.querySelector('form').reset();
+    popUp.classList.remove('display-form');
+
+    myLibrary.push(book);
+}
+
+function appendBook() {
+    for(let appendNum = 0; appendNum < myLibrary.length; appendNum++) {
+        let newDiv = document.createElement('div');
+        this.author = document.createElement('p');
+        this.author.innerText = `Author: ${myLibrary[appendNum].author}`;
+        
+        this.pages = document.createElement('p');
+        this.pages.innerText = `Pages: ${myLibrary[appendNum].pages}`;
+    
+        this.read = document.createElement('p');
+        this.read.innerText = `Read: ${myLibrary[appendNum].read}`;
+    
+        this.title = document.createElement('p');
+        this.title.innerText = `Title: ${myLibrary[appendNum].title}`;
+    
+        newDiv.classList.add('book-card');
+        newDiv.appendChild(title);
+        newDiv.appendChild(pages);
+        newDiv.appendChild(author);
+        newDiv.appendChild(read);
+    
+        bookContainer.appendChild(newDiv);
+    }
+}
+
+//Waits for button click, then pushes form to array
+document.addEventListener('DOMContentLoaded', () => {
+    submitForm.addEventListener('click', (e) => {
+        e.preventDefault();
+        addBookToLibrary();
+        appendBook();
+        readStatus = undefined;
+    });
+});
+
+//opens the form pop up when clicked
 addBtn.addEventListener('click', () => {
     if(popUp.classList.contains('display-form')) {
         popUp.classList.remove('display-form');
@@ -11,9 +78,7 @@ addBtn.addEventListener('click', () => {
     }
 });
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+//Changes read button to true or false
+readBtnYes.addEventListener('click', () => readStatus = 'Yes');
+
+readBtnNo.addEventListener('click', () => readStatus = 'No');
